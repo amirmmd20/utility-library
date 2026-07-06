93f3866b2c8391513cf3e5ce6fc21c1cf21866af
mobinamoeini400 <mobinamoeini400@example.com>
2026-07-06 21:37:00 +0200

    added reverseString function

--- functions.c
+++ functions.c
@@ -1,1 +1,14 @@
+#include <string.h>
 
+void reverseString(char str[]){
+    int i=0;
+    int j= strlen(str)-1;
+    char t;
+    while(i<j){
+        t=str[i];
+        str[i]=str[j];
+        str[j]=t;
+        i++;
+        j--;
+    }
+}
--- functions.h
+++ functions.h
@@ -1,1 +1,6 @@
+#ifdef FUNCTION_H
+#define FUNCTION_H
 
+void reverseString(char str[]);
+
+#endif
